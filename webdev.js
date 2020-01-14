efa0f4b199870e06d5d84ef3d4f6ff434220debe
Giulio Bonanome <1076656+gbonanome@users.noreply.github.com>
2020-01-14 23:47:35 +0100

    Aggiunti commenti al file JS

--- webdev.js
+++ webdev.js
@@ -1,61 +1,80 @@
+// Tutto il codice all'interno di questa funzione viene 
+// eseguito quando la pagina ha terminato il caricamento
 window.addEventListener( "load", function () {
+
+	// La funzione che invia i dati al webserver
 	function sendData(method, action) {
+
+		// Creiamo la richiesta HTTP
 		const XHR = new XMLHttpRequest();
 
-		// Bind the FormData object and the form element
+		// Costruiamo un oggetto FormData che contiene il nostro form
 		const FD = new FormData( form );
 
-		// Define what happens on successful data submission
+		// Qui dentro c'è tutto quello che succede quando 
+		// l'invio di dati avviene con successo
 		XHR.addEventListener( "load", function(event) {
-			// Log the response to help debug
+			
+			// Stampo nella console la risposta dal server 
+			// per un eventuale debug. La risposta potrebbe essere
+			// corretta ma potrei avere errori di altro tipo dal DB
 			console.log(event.target.responseText);
 
+			// Seleziono il div dove stampare i risultati
 			const div = document.querySelector('#result');
-			// Clear previous results if present
+			
+			// Rimuovo tutto il contenuto del div con id result,
+			// nel caso in cui stessi facendo una nuova ricerca
 			while (div.firstChild) {
 			  div.removeChild(div.firstChild);
 			}
 
+			// Creo un oggetto che contiene la risposta JSON del webserver
 			const resp = JSON.parse(event.target.responseText);
 
 			if (resp.length === 0) {
-				// What to do if we do NOT have any result
+				// Cosa succede se ho 0 risultati
 				div.innerHTML = "<p>Nessun risultato</p>"
 			}
 			else {
-				// What to do if we HAVE have some results
+				// Cosa succede se c'è uno o più risultati
 
+				// Utilizzo un ciclo per scorrere tutti i risultati
 				for (var i = 0; i < resp.length; i++) {
-					// Create a paragraph under #result div
+					// Creo un elemento paragrafo
 					const para = document.createElement('p');
-					// Insert a string with name and time
+					// Come testo del paragrafo concateno 
+					// le stringhe Nome e Tempo
 					para.textContent = resp[i].Nome + " " + resp[i].Tempo;
-					// Print <p> tags under #result div
+					// Inserisco nel div dei risultati
+					// il paragrafo appena creato
 					div.appendChild(para);
 				}
 			}
 
 		} );
 
-		// Define what happens in case of error
+		// Cosa succede quando l'invio dei dati non va a buon fine
 		XHR.addEventListener( "error", function( event ) {
 			console.log( 'Oops! Something went wrong.' );
 		} );
 
-		// Set up our request
+		// Viene impostata la connessione
 		XHR.open( method, action );
 
-		// The data sent is what the user provided in the form
+		// Vengono inviati i dati contenuti nel form 
 		XHR.send( FD );
 	}
 
-	// Access the form element...
+	// Selezioniamo l'elemento con id searchForm
 	let form = document.getElementById( "searchForm" );
 
-	// ...and take over its submit event.
+	// e associamo un evento all'invio del modulo
+	// ovvero quando viene premuto il tasto submit
 	form.addEventListener( "submit", function ( event ) {
 		event.preventDefault();
 
+		// l'evento sarà la nostra funzione sendData (vedi sopra)
 		sendData(form.method, form.action);
 	} );
 } );
