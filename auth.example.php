976f7f3cc97bacd8dbb0f6ff840835ef2b907777
Giulio Bonanome <1076656+gbonanome@users.noreply.github.com>
2020-01-16 11:22:47 +0100

    Aggiunti commenti anche per i parametri di connessione

--- auth.example.php
+++ auth.example.php
@@ -1,10 +1,10 @@
 <?php
 
-    $host = '127.0.0.1';
-    $user = 'root';
-    $password = '';
-    $name = 'Hogwarts';
-    $port = 8889;
-    $socket = '';
+    $host = '127.0.0.1'; // l'indirizzo IP del vostro computer
+    $user = 'root'; // l'utente che può accedere al vostro DB
+    $password = ''; // la password impostata per accedere al vostro DB, se non l'avete mai impostata potrebbe essere 'root' oppure niente ''
+    $name = 'Hogwarts'; // il nome del database a cui vogliamo connetterci
+    $port = 8889; // la porta virtuale di accesso tramite webserver
+    $socket = ''; // il socket di connessione, se usate MAMP potrebbe essere una cosa del tipo 'localhost:/Applications/MAMP/tmp/mysql/mysql.sock'
 
 ?>
